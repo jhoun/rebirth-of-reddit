794349315c8e8199f70c437d2a6a5391116bd9db
jhoun <jayhoun@gmail.com>
2016-12-23 19:36:16 -1000

    requested date, time, author from XML request

--- public/js/app.js
+++ public/js/app.js
@@ -55,25 +55,72 @@ content.appendChild(postContainer);
   post.classList.add('post');
   postContainer.appendChild(post);
 
-    //Creates <img> from post
-    var imgPost = document.createElement('img');
-    imgPost.src ="asdf";
-    post.appendChild(imgPost)
+    //Creats "imgContainer" from content
+    var imgContainer = document.createElement('div');
+    imgContainer.classList.add('imgContainer');
+    post.appendChild(imgContainer);
 
-    //Creates "postHeader" from post
-    var postHeader = document.createElement('div');
-    postHeader.classList.add('postHeader');
-    postHeader.innerHTML = "header here";
-    post.appendChild(postHeader);
+//Request to get img
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", getImg);
+oReq.open("GET", "https://www.reddit.com/r/aww/.json");
+oReq.send();
+
+//Get Image function
+function getImg(){
+  var aww = JSON.parse(this.responseText);
+  console.log(aww);
+  var url = aww.data.children[2].data.url;
+  var isGif = /gifv/i.test(url);
+  if (isGif){
+    var url = url.replace(/gifv/i, 'gif');
+  }
+
+  console.log(url);
+
+  //Creates <img> from post
+  var imgPost = document.createElement('img');
+  imgPost.src = url;
+  imgContainer.appendChild(imgPost)
+
+}
+
+//Request to get title
+var oReq2 = new XMLHttpRequest();
+oReq2.addEventListener("load", getHeader);
+oReq2.open("GET", "https://www.reddit.com/r/aww/.json");
+oReq2.send();
+
+//Get Header function
+function getHeader(){
+  var aww = JSON.parse(this.responseText);
+  //Creates "postHeader" from post
+  var postHeader = document.createElement('div');
+  postHeader.classList.add('postHeader');
+  postHeader.innerHTML = aww.data.children[2].data.title;
+  post.appendChild(postHeader);
+}
+
+var oReqAuthor = new XMLHttpRequest();
+oReq2.addEventListener("load", getAuthor);
+oReq2.open("GET", "https://www.reddit.com/r/aww/.json");
+oReq2.send();
+
+function getAuthor(){
+    var aww = JSON.parse(this.responseText);
+    var author = aww.data.children[2].data.author;
+    var date = aww.data.children[2].data.created;
+    var date = new Date( date *1000);
 
     //Creates "postMetaData" from post
     var postMetaData = document.createElement('div');
     postMetaData.classList.add('postMetaData');
-    postMetaData.innerHTML = "Author/ Date Posted / views";
+    postMetaData.innerHTML = "by " + author + " * " + date ;
     post.appendChild(postMetaData);
-
+}
     //Creates "postContent" from post
     var postContent = document.createElement('div');
     postContent.classList.add('postContent');
     postContent.innerHTML = "Content Here,Content Here,Content Here,Content Here,Content Here ";
     post.appendChild(postContent);
+
