68142e933dc3eefec343f81a2b223122986c0bb0
jhoun <jayhoun@gmail.com>
2016-12-23 15:48:13 -1000

    completed navagation bar in DOM

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,43 @@
-console.log('sanity check')
+//Navigation Area
+var topContainer = document.getElementById('topContainer');
+
+//Creates "header" <div> from topContainer
+var header = document.createElement('div')
+header.id = 'header';
+topContainer.appendChild(header);
+
+  //Creates <img> from header
+  var imgLogo = document.createElement('img');
+  imgLogo.src = "./assets/logo.svg";
+  header.appendChild(imgLogo);
+
+  //Creates "plusBtn" from header
+  var plusBtn = document.createElement('div');
+  plusBtn.id = 'plusBtn';
+  plusBtn.innerHTML = '+';
+  header.appendChild(plusBtn);
+
+//Creates <nav> from topContainer
+var nav = document.createElement('nav');
+topContainer.appendChild(nav);
+
+  //Creates "random" from nav
+  var random = document.createElement('div')
+  random.innerHTML = 'RANDOM';
+  random.id = 'random';
+  random.classList.add('font');
+  nav.appendChild(random);
+
+  //Creates "myBoard" from nav
+  var myBoard = document.createElement('div')
+  myBoard.innerHTML = 'MY BOARD';
+  myBoard.id = 'myBoard';
+  myBoard.classList.add('font');
+  nav.appendChild(myBoard);
+
+  //Creates "getApp" from nav
+  var getApp = document.createElement('div')
+  getApp.innerHTML = 'GET THE APP';
+  getApp.id = 'getApp';
+  getApp.classList.add('font');
+  nav.appendChild(getApp);
