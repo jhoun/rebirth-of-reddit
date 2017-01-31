b63e606faa6553a7bc0a11746a434759f7394166
jhoun <jayhoun@gmail.com>
2017-01-30 14:14:37 -1000

    removed gifv to show gif

--- public/js/controller.js
+++ public/js/controller.js
@@ -7,9 +7,20 @@
     'Reddit',
     function($scope, Reddit){
     $scope.reddit = [];
-      Reddit.getReddit().then(function(reddit){
-      $scope.reddit = reddit.data.data.children;
-      console.log($scope.reddit);
+      Reddit.getReddit().then(function(posts){
+
+        // var posts = reddit.data.data.children.map(function(post) {
+        //   if (/gifv/i.test(post.data.url)) {
+        //     post.data.url = post.data.url.replace(/gifv/i, 'gif');
+        //     return post
+        //   } else {
+        //     return post;
+        //   }
+        // })
+
+
+        $scope.reddit = posts;
+
       });
   }])
 
--- public/js/providers.js
+++ public/js/providers.js
@@ -9,7 +9,17 @@ angular.module('app')
       var endpoint = this.endpoint;
       return {
         getReddit : function(){
-          return $http.get(endpoint);
+          return $http.get(endpoint).then(function(reddit){
+
+            return reddit.data.data.children.map(function(post) {
+              if (/gifv/i.test(post.data.url)) {
+                post.data.url = post.data.url.replace(/gifv/i, 'gif');
+                return post
+              } else {
+                return post;
+              }
+            })
+          })
         }
       };
     }];
