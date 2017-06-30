5215b8800d599816c1d8de862205e0fdc21ff00e
jhoun <jayhoun@gmail.com>
2017-06-29 21:17:59 -0700

    added date

--- public/js/controller.js
+++ public/js/controller.js
@@ -11,9 +11,11 @@
         .then(function(res){
           res.data.data.children.shift();
           var posts = res.data.data.children.map(function(post) {
+            var redditDate = post.data.created;
+            post.data.created = new Date( redditDate * 1000);
             if (/gifv/i.test(post.data.url)) {
               post.data.url = post.data.url.replace(/gifv/i, 'gif');
-              return post
+              return post;
             } else {
               return post;
             }
